32c102a2a4d23e13990cdc6b33a3a5be2badd333
BarbaraVictoriaDias <barbara.v.dias@aluno.senai.br>
2024-11-05 15:46:47 -0300

    login

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o envio do formulário
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('errorMessage');
+
+    // Validação simples (substitua por autenticação real conforme necessário)
+    if (username === 'pumasmartcore@gmail.com' && password === '311024') {
+        // Aqui você pode redirecionar o usuário para outra página
+        window.location.href = "home.html";
+    } else {
+        errorMessage.textContent = 'Usuário ou senha inválidos.';
+    }
+});
